c567de1344445ff855b611b6c5fe8d0c25f94fe0
gautam dheer <gautam.litesh@gmail.com>
2025-01-09 20:03:23 +0530

    added css file

--- js-todo-app/script.js
+++ js-todo-app/script.js
@@ -1,1 +1,13 @@
- 
+ const input = document.getElementById('todo-input');
+ const form = document.getElementById('todo-form');
+ const list = document.getElementById('todo-list');
+
+
+ form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const todoItem = input.value;
+    const li = document.createElement('li');
+    li.textContent = todoItem;
+    list.appendChild(li);
+    input.value = '';
+ });
